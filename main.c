7adeadd807c515688e4f1071856413410ad42ae5
Toby Alden <toby.alden@gmail.com>
2015-08-10 12:40:21 -0700

    Working version with pointers-to-pointers.

--- main.c
+++ main.c
@@ -4,18 +4,21 @@
 #include <string.h>
 #include <math.h>
 
-void print_array(char array[], int length);
-void hex_to_base64(char *hex);
+void print_array(char array[], long length);
+long hex_to_base64(char *hex, char **base64);
 
 const char base64_table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 int main(int argc, char *argv[])
 {
   printf("HEX INPUT: %s\n", argv[1]);
-  hex_to_base64(argv[1]);
+  char *base64;
+  long length = hex_to_base64(argv[1], &base64);
+  printf("BASE64 OUTPUT: ");
+  print_array(base64, length);
 }
 
-void hex_to_base64(char *hex) {
+long hex_to_base64(char *hex, char **base64) {
   mpz_t decimal;
   mpz_init_set_str (decimal, hex, 16);
   mpz_t base;
@@ -30,7 +33,9 @@ void hex_to_base64(char *hex) {
   }
 
   long length = power;
-  char base64[length];
+  // char base64_arr[length];
+  char *base64_arr;
+  base64_arr = (char *) malloc(length);
 
   if(power > 0) {
     power--;
@@ -43,7 +48,7 @@ void hex_to_base64(char *hex) {
     mpz_fdiv_q(quotient, decimal, place_value);
     unsigned long quotient_ui = mpz_get_ui(quotient);
     char place_char = base64_table[quotient_ui];
-    base64[(length - 1) - power] = place_char;
+    base64_arr[(length - 1) - power] = place_char;
     mpz_t decrement;
     mpz_init(decrement);
     mpz_mul_ui(decrement, place_value, quotient_ui);
@@ -51,12 +56,13 @@ void hex_to_base64(char *hex) {
     power--;
   }
 
-  printf("BASE64 OUTPUT: ");
-  print_array(base64, length);
+  *base64 = &base64_arr[0];
+  return length;
 }
 
-void print_array(char array[], int length) {
-  for (int i = 0; i < length; i++) {
+void print_array(char *array, long length) {
+  // printf("firstchar: %c\n", array[0]);
+  for (long i = 0; i < length; i++) {
     printf("%c", array[i]);
   }
   printf("\n");
